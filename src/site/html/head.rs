cb6e184976d6dbf81ff2abf63a60301ccfce4422
Sara Vieira <hey@iamsaravieira.com>
2022-12-12 18:44:04 -0500

    fix - fix description showing in body

--- src/site/html/head.rs
+++ src/site/html/head.rs
@@ -1,20 +1,21 @@
 use crate::config::{theme, Config};
 
 pub fn head(config: &Config) -> String {
+    println!("{:?}", &config.description);
     format!(
         r#"
    <!DOCTYPE html>
    <html lang="en">
    <head>
    
-   <meta charset="utf-8">
+   <meta charset="utf-8" />
    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
    <link rel="stylesheet" href="styles.css"></link>
    
    <title>{name}</title>
-   <meta name="description" content={description} />
-    <meta property="og:url" content={homepage}>
+   <meta name="description" content="{description}">
+    <meta property="og:url" content="{homepage}">
    </head>
    <body>
    <div id="oranda"><div class="body {theme}"><div class="container">
