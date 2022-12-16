0233975760d8c16f0b48e660b4b593f98b05d8b3
Sara Vieira <hey@iamsaravieira.com>
2022-12-16 14:58:03 -0500

    simplify

--- src/site/html/logo.rs
+++ src/site/html/logo.rs
@@ -6,47 +6,50 @@ use std::{
 
 use crate::{config::Config, errors::*};
 
-pub fn get_logo(config: &Config) -> Result<&PathBuf> {
+pub fn get_logo(config: &Config) -> Result<Option<&PathBuf>> {
     let mut logo_url;
-    if !config.logo.is_empty() {
-        if config.logo.starts_with("http") {
-            let resp = reqwest::blocking::get(&config.logo);
 
-            match resp {
-                Err(_) => {
-                    return Err(OrandaError::RequestFailed {
-                        url: config.logo.to_string(),
-                        resource: String::from("Logo"),
-                    });
-                }
-                Ok(img) => {
-                    let logo_path = Path::join(
-                        Path::new(&config.dist_dir),
-                        Path::new(&config.logo).file_name().unwrap(),
-                    );
-
-                    let mut logo_file = File::create(&logo_path)?;
-                    logo_file.write_all(&img.bytes().unwrap())?;
-
-                    logo_url = &logo_path;
-                }
-            }
-        } else {
-            if !Path::new(&config.logo).exists() {
-                return Err(OrandaError::FileNotFound {
-                    filedesc: "Logo".to_owned(),
-                    path: config.logo.to_owned(),
-                });
-            }
-
-            let new_path = Path::join(
-                Path::new(&config.dist_dir),
-                Path::new(&config.logo).file_name().unwrap(),
-            );
-            fs::copy(&config.logo, &new_path).unwrap();
-            logo_url = &new_path;
-        }
+    if config.logo.is_empty() {
+        return Ok(None);
     }
 
-    Ok(&logo_url)
+    if config.logo.starts_with("http") {
+        let resp = reqwest::blocking::get(&config.logo);
+
+        match resp {
+            Err(_) => {
+                return Err(OrandaError::RequestFailed {
+                    url: config.logo.to_string(),
+                    resource: String::from("Logo"),
+                });
+            }
+            Ok(img) => {
+                let logo_path = Path::join(
+                    Path::new(&config.dist_dir),
+                    Path::new(&config.logo).file_name().unwrap(),
+                );
+
+                let mut logo_file = File::create(&logo_path)?;
+                logo_file.write_all(&img.bytes().unwrap())?;
+
+                logo_url = &logo_path;
+            }
+        }
+    } else {
+        if !Path::new(&config.logo).exists() {
+            return Err(OrandaError::FileNotFound {
+                filedesc: "Logo".to_owned(),
+                path: config.logo.to_owned(),
+            });
+        }
+
+        let new_path = Path::join(
+            Path::new(&config.dist_dir),
+            Path::new(&config.logo).file_name().unwrap(),
+        );
+        fs::copy(&config.logo, &new_path).unwrap();
+        logo_url = &new_path;
+    }
+
+    Ok(Some(&logo_url))
 }
