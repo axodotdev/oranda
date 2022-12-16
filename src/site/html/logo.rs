6a91fd63d5a0d0e8d711e2417784cdace6951a29
Sara Vieira <hey@iamsaravieira.com>
2022-12-16 15:16:47 -0500

    move remote logo fetching

--- src/site/html/logo.rs
+++ src/site/html/logo.rs
@@ -7,6 +7,27 @@ use std::{
 use crate::config::Config;
 use crate::errors::*;
 
+fn get_remote_logo(logo: &String, dist_dir: &String) -> Result<PathBuf> {
+    let resp = reqwest::blocking::get(logo);
+
+    match resp {
+        Err(_) => {
+            return Err(OrandaError::RequestFailed {
+                url: logo.to_string(),
+                resource: String::from("Logo"),
+            });
+        }
+        Ok(img) => {
+            let logo_path = Path::join(Path::new(dist_dir), Path::new(&logo).file_name().unwrap());
+
+            let mut logo_file = File::create(&logo_path)?;
+            logo_file.write_all(&img.bytes().unwrap())?;
+
+            Ok(logo_path)
+        }
+    }
+}
+
 pub fn get_logo(config: &Config) -> Result<Option<&PathBuf>> {
     if !config.logo.is_none() {
         return Ok(None);
@@ -16,27 +37,8 @@ pub fn get_logo(config: &Config) -> Result<Option<&PathBuf>> {
     let logo = Option::expect(config.logo, "hmm, shouldn't really happen");
 
     if logo.starts_with("http") {
-        let resp = reqwest::blocking::get(&logo);
-
-        match resp {
-            Err(_) => {
-                return Err(OrandaError::RequestFailed {
-                    url: logo.to_string(),
-                    resource: String::from("Logo"),
-                });
-            }
-            Ok(img) => {
-                let logo_path = Path::join(
-                    Path::new(&config.dist_dir),
-                    Path::new(&logo).file_name().unwrap(),
-                );
-
-                let mut logo_file = File::create(&logo_path)?;
-                logo_file.write_all(&img.bytes().unwrap())?;
-
-                logo_url = Some(&logo_path)
-            }
-        }
+        let remote_fetched_path = get_remote_logo(&logo, &config.dist_dir).unwrap();
+        logo_url = Some(&remote_fetched_path);
     } else {
         if !Path::new(&logo).exists() {
             return Err(OrandaError::FileNotFound {
