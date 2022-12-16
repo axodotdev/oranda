8ce343abed37ded6d02ce9b4b1c9f0ffb2f41801
Ashley Williams <ashley666ashley@gmail.com>
2022-12-16 14:39:18 -0600

    fix(html): remove uneeded meta tag

--- src/site/html.rs
+++ src/site/html.rs
@@ -10,7 +10,6 @@ pub fn build(config: &Config, content: String) -> String {
     <head>
     <title>{ text!(&config.name) }</title>
     <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta name="description" content=&config.description />
     <link rel="stylesheet" href="styles.css"></link>
