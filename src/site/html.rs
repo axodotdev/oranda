404d460844a5c5f1efb2e5f25bf798e36df87e4f
Sara Vieira <hey@iamsaravieira.com>
2022-12-19 14:52:37 +0000

    add homepage tag back

--- src/site/html.rs
+++ src/site/html.rs
@@ -6,6 +6,12 @@ pub fn build(config: &Config, content: String) -> String {
     let theme = theme::css_class(&config.theme);
     let classlist: &str = &format!("body container {}", theme)[..];
     let description = &config.description;
+    let homepage = match &config.homepage {
+        Some(homepage) => Some(html!(
+            <meta property="og:url" content=homepage/>
+        )),
+        None => None,
+    };
 
     let doc: DOMTree<String> = html!(
     <html lang="en" id="oranda">
@@ -13,7 +19,9 @@ pub fn build(config: &Config, content: String) -> String {
     <title>{ text!(&config.name) }</title>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    { homepage }
     <meta name="description" content=description />
+    <meta property="og:description" content=description/>
     <link rel="stylesheet" href="styles.css"></link>
     </head>
     <body>
