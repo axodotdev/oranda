0dd941ec80ddf665764cdc021a8b8c79196bc963
Sara Vieira <hey@iamsaravieira.com>
2022-12-22 14:26:01 +0000

    clean up analytics fetching

--- src/config/analytics.rs
+++ src/config/analytics.rs
@@ -6,102 +6,78 @@ use axohtml::html;
 use serde::Deserialize;
 
 #[derive(Debug, Deserialize)]
-struct GoogleTracking {
+pub struct GoogleTracking {
     pub tracking_id: String,
 }
 
 #[derive(Debug, Deserialize)]
-struct FathomTracking {
+pub struct FathomTracking {
     pub site: String,
 }
 
 #[derive(Debug, Deserialize)]
-struct PlausibleTracking {
+pub struct PlausibleTracking {
     pub domain: String,
     pub script_url: Option<String>,
 }
 
 #[derive(Debug, Deserialize)]
-struct UnamiTracking {
+pub struct UnamiTracking {
     pub website: String,
     pub script_url: String,
 }
 
 #[derive(Debug, Deserialize)]
-pub struct Analytics {
-    google: Option<GoogleTracking>,
-    plausible: Option<PlausibleTracking>,
-    fathom: Option<FathomTracking>,
-    unami: Option<UnamiTracking>,
+#[serde(rename_all = "lowercase")]
+pub enum Analytics {
+    Google(GoogleTracking),
+    Plausible(PlausibleTracking),
+    Fathom(FathomTracking),
+    Unami(UnamiTracking),
 }
 
 const GOOGLE_SCRIPT_URL: &str = "https://www.googletagmanager.com/gtag/js";
 const PLAUSIBLE_SCRIPT_URL: &str = "https://plausible.io/js/script.js";
 const FATHOM_SCRIPT_URL: &str = "https://cdn.usefathom.com/script.js";
 
-pub fn get_google_script(config: &Config) -> Box<script<String>> {
-    return config
-        .analytics
-        .as_ref()
-        .map(|analytics_type| {
-            analytics_type
-                .google
-                .as_ref()
-                .map(|g| html!(<script>{format!("window.dataLayer = window.dataLayer || []; function gtag(){{dataLayer.push(arguments);}} gtag('js', new Date());gtag('config', {});", g.tracking_id)}</script>))
-        })
-        .unwrap()
-        .unwrap();
-}
-pub fn get_analytics(config: &Config) -> Result<Box<script<String>>> {
+// pub fn get_google_script(config: &Config) -> Box<script<String>> {
+//     return config
+//         .analytics
+//         .as_ref()
+//         .map(|analytics_type| {
+//             analytics_type
+//                 .google
+//                 .as_ref()
+//                 .map(|g| html!(<script>{format!("window.dataLayer = window.dataLayer || []; function gtag(){{dataLayer.push(arguments);}} gtag('js', new Date());gtag('config', {});", g.tracking_id)}</script>))
+//         })
+//         .unwrap()
+//         .unwrap();
+// }
+pub fn get_analytics(config: &Config) -> Option<Box<script<String>>> {
     let analytics = config.analytics.as_ref();
-    let script = analytics.map(|analytics_type| {
-        let fathom = analytics_type.fathom.as_ref();
-        let plausible = analytics_type.plausible.as_ref();
-        let google = analytics_type.google.as_ref();
-        let unami = analytics_type.unami.as_ref();
 
-        if fathom.is_none() && plausible.is_none() && google.is_none() && unami.is_none() {
-            return Err(OrandaError::Other(
-                "Invalid configuration, you need to choose an analytics type".to_string(),
-            ));
-        }
-
-        if fathom.is_some() {
-            let site = fathom.unwrap();
-            return Ok(html!(
-                <script defer=true src=FATHOM_SCRIPT_URL data-site=&site.site ></script>
-            ));
-        }
-
-        if unami.is_some() {
-            let site = unami.unwrap();
-            return Ok(html!(
-                <script
-                    async=true
-                    defer=true
-                    src=&site.script_url
-                    data-website-id=&site.website
-                ></script>
-            ));
-        }
-
-        if google.is_some() {
-            let site = google.unwrap();
-            let script_url = format!("{}?id={}", GOOGLE_SCRIPT_URL, site.tracking_id);
-            return Ok(html!(
-                <script async=true src=&script_url></script>
-
-            ));
-        }
-
-        let site = plausible.unwrap();
-        let url = PLAUSIBLE_SCRIPT_URL.to_string();
-        let script_url = site.script_url.as_ref().unwrap_or(&url);
-        return Ok(html!(
-            <script defer=true data-domain=&site.domain src=script_url></script>
-
-        ));
-    });
-
-    return script.unwrap();
+    match analytics {
+        None => None,
+        Some(analytics) => match analytics {
+            Analytics::Fathom(f) => {
+                Some(html!(<script defer=true src=FATHOM_SCRIPT_URL data-site=&f.site ></script>))
+            }
+            Analytics::Unami(u) => Some(
+                html!(<script async=true defer=true src=&u.script_url data-website-id=&u.website></script>),
+            ),
+            Analytics::Google(g) => {
+                let script_url = format!("{}?id={}", GOOGLE_SCRIPT_URL, g.tracking_id);
+                Some(html!(
+                    <script async=true src=&script_url></script>
+                ))
+            }
+            Analytics::Plausible(p) => {
+                let url = PLAUSIBLE_SCRIPT_URL.to_string();
+                let script_url = p.script_url.as_ref().unwrap_or(&url);
+                Some(html!(
+                    <script defer=true data-domain=&p.domain src=script_url></script>
+                ))
+            }
+        },
+    }
 }
