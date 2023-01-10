9516f66439a0f7e2ef416536dafddedb57e75b16
Sara Vieira <hey@iamsaravieira.com>
2023-01-10 16:43:21 +0000

    always have dark class

--- src/config/theme.rs
+++ src/config/theme.rs
@@ -7,12 +7,15 @@ pub enum Theme {
     Dark,
     #[serde(rename = "axo-dark")]
     AxoDark,
+    #[serde(rename = "axo-light")]
+    AxoLight,
 }
 
 pub fn css_class(theme: &Theme) -> &str {
     match theme {
         Theme::Dark => "dark",
-        Theme::AxoDark => "axo-dark",
+        Theme::AxoDark => "axo dark",
+        Theme::AxoLight => "axo light",
         _ => "light",
     }
 }
