a152e1a2e3298b516af40a5a8be5604e2692ed7e
Sara Vieira <hey@iamsaravieira.com>
2023-01-17 17:27:10 +0100

    remove hardcoded classes

--- src/config/header.rs
+++ src/config/header.rs
@@ -11,8 +11,7 @@ pub fn create_header(config: &Config) -> Option<Box<header<String>>> {
     }
     let nav = match config.additional_pages.as_ref() {
         Some(pages) => {
-            let mut html: Vec<Box<li<String>>> =
-                vec![html!(<li style="margin: 0" class="capitalize"><a href="/">"Home"</a></li>)];
+            let mut html: Vec<Box<li<String>>> = vec![html!(<li><a href="/">"Home"</a></li>)];
             for page in pages.iter() {
                 let path = Path::new(page);
                 let file_name = path
@@ -20,13 +19,11 @@ pub fn create_header(config: &Config) -> Option<Box<header<String>>> {
                     .unwrap_or(path.as_os_str())
                     .to_string_lossy();
                 let path = format!("/{}", file_name);
-                html.extend(
-                    html!(<li style="margin: 0" class="capitalize"><a href=path>{text!(file_name)}</a></li>),
-                );
+                html.extend(html!(<li><a href=path>{text!(file_name)}</a></li>));
             }
             Some(html!(
-            <nav class="p-0 text-center mb-8">
-                <ul class="p-0 flex gap-6 items-center text-center list-none justify-center">
+            <nav class="nav">
+                <ul>
                      {html}
                 </ul>
             </nav>
