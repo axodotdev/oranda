2d011b2c856c59216b3ad9ad4d6ea8045b0e3b30
Sara Vieira <hey@iamsaravieira.com>
2023-01-20 12:55:33 +0100

    fix tests

--- src/site/mod.rs
+++ src/site/mod.rs
@@ -1,4 +1,5 @@
 use crate::errors::*;
+
 use std::fs::File;
 use std::io::Write;
 use std::path::Path;
@@ -6,10 +7,13 @@ mod head;
 mod header;
 mod html;
 pub mod markdown;
+use crate::config::Config;
 
 #[cfg(test)]
 use crate::config::theme::Theme;
-use crate::config::Config;
+
+#[cfg(test)]
+use crate::initialize_tokio_runtime;
 
 #[derive(Debug)]
 pub struct Site {
@@ -82,29 +86,33 @@ fn config() -> Config {
     }
 }
 
-#[tokio::test]
-async fn it_builds_the_site() {
+#[test]
+fn it_builds_the_site() {
+    let _guard = initialize_tokio_runtime().enter();
     let site = Site::build(&config(), &config().readme_path).unwrap();
     assert!(site.html.contains("<h1>axo</h1>"));
     assert!(site.html.contains("axo-oranda.css"));
 }
 
-#[tokio::test]
-async fn reads_description() {
+#[test]
+fn reads_description() {
+    let _guard = initialize_tokio_runtime().enter();
     let site = Site::build(&config(), &config().readme_path).unwrap();
     println!("{:?}", site.html);
     assert!(site.html.contains("you axolotl questions"));
     assert!(site.html.contains("My Axo project"))
 }
 
-#[tokio::test]
-async fn reads_theme() {
+#[test]
+fn reads_theme() {
+    let _guard = initialize_tokio_runtime().enter();
     let site = Site::build(&config(), &config().readme_path).unwrap();
     assert!(site.html.contains("html class=\"dark\""));
 }
 
-#[tokio::test]
-async fn creates_nav() {
+#[test]
+fn creates_nav() {
+    let _guard = initialize_tokio_runtime().enter();
     let site = Site::build(&config(), &config().readme_path).unwrap();
 
     assert!(site.html.contains("<nav class=\"nav\"><ul><li><a href=\"/\">Home</a></li><li><a href=\"/readme\">readme</a></li></ul></nav>"));
