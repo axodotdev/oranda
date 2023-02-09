c8f11600b62166a05452a0575be1d45d6f2cd608
Sara Vieira <hey@iamsaravieira.com>
2023-02-09 16:52:43 +0100

    Only adds  JS to home and artifacts (#172)

--- src/site/page.rs
+++ src/site/page.rs
@@ -14,23 +14,26 @@ pub struct Page {
     pub contents: String,
     pub filename: String,
     pub is_index: bool,
+    pub needs_js: bool,
 }
 
 impl Page {
-    pub fn new_from_file(config: &Config, source: &str) -> Result<Self> {
+    pub fn new_from_file(config: &Config, source: &str, needs_js: bool) -> Result<Self> {
         let is_index = source == config.readme_path;
         Ok(Page {
             contents: Self::load_and_render_contents(source, &config.syntax_theme)?,
             filename: Self::filename(source, is_index),
             is_index,
+            needs_js,
         })
     }
 
-    pub fn new_from_contents(contents: String, filename: &str) -> Self {
+    pub fn new_from_contents(contents: String, filename: &str, needs_js: bool) -> Self {
         Page {
             contents,
             filename: filename.to_string(),
             is_index: false,
+            needs_js,
         }
     }
 
@@ -39,7 +42,7 @@ impl Page {
         markdown::to_html(contents, syntax_theme)
     }
 
-    pub fn build(self, config: &Config) -> Result<String> {
+    pub fn build(self, config: &Config, needs_js: bool) -> Result<String> {
         let page_contents = if self.is_index {
             let artifacts_header = artifacts::build_header(config)?;
             html!(<div>{artifacts_header}{unsafe_text!(self.contents)}</div>).to_string()
@@ -47,7 +50,7 @@ impl Page {
             let html: Box<div<String>> = html!(<div>{unsafe_text!(self.contents)}</div>);
             html.to_string()
         };
-        layout::build(config, page_contents)
+        layout::build(config, page_contents, needs_js)
     }
 
     pub fn filename(source: &str, is_index: bool) -> String {
