3a5cd92ca08fda000f8dbd88710168fdd3dac788
Sara Vieira <hey@iamsaravieira.com>
2023-02-09 01:25:11 +0100

    create dir

--- src/site/artifacts/cargo_dist.rs
+++ src/site/artifacts/cargo_dist.rs
@@ -1,7 +1,7 @@
 use crate::config::Config;
 use crate::errors::*;
-use crate::site::link;
 use crate::site::markdown::syntax_highlight;
+use crate::site::{link, Site};
 use axohtml::dom::UnsafeTextNode;
 use axohtml::elements::{div, span};
 use axohtml::{html, text, unsafe_text};
@@ -29,6 +29,7 @@ fn get_installer_path(config: &Config, name: &String) -> Result<String> {
     let file_string_future = axoasset::load_string(download_link.as_str());
     let file_string = tokio::runtime::Handle::current().block_on(file_string_future)?;
     let file_path = format!("{}.txt", &name);
+    Site::create_dist_dir(&config.dist_dir)?;
     let asset = axoasset::local::LocalAsset::new(
         format!("{}/{}", &config.dist_dir, &file_path).as_str(),
         file_string.as_bytes().to_vec(),
