60e89185f7119a1445ed2d73ada6db3cd6b575d7
ashley williams <1163554+ashleygwilliams@users.noreply.github.com>
2023-02-27 08:08:09 -0600

    fix(err): add fetch and parse errors for cargo dist manifest (#191)

--- src/errors.rs
+++ src/errors.rs
@@ -34,6 +34,15 @@ pub enum OrandaError {
     #[error("Could not find a build in {dist_dir}. Did you remember to run `oranda build`?")]
     BuildNotFound { dist_dir: String },
 
+    #[error("Encountered an error (status: {status_code}) while fetching your cargo-dist release manifest at {url}. This often occurs when you haven't yet published a GitHub release for the version set in your Cargo.toml. Consider publishing a release or promoting a draft release for the current version, or updating your Cargo.toml to use an already released version.")]
+    CargoDistManifestFetchError {
+        url: String,
+        status_code: reqwest::StatusCode,
+    },
+
+    #[error("Encountered an error parsing your cargo-dist manifest at {url}. Details: {details}")]
+    CargoDistManifestParseError { url: String, details: String },
+
     #[error("{0}")]
     Other(String),
 }
