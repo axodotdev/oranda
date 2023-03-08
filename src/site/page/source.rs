1407591dab88e6a59ba4b81d58568d4371616021
Sara Vieira <hey@iamsaravieira.com>
2023-03-08 15:34:12 +0100

    only use one arm

--- src/site/page/source.rs
+++ src/site/page/source.rs
@@ -4,6 +4,6 @@ pub fn is_markdown(file: &str) -> bool {
     let file_path = Path::new(&file);
     match file_path.extension() {
         None => false,
-        Some(ext) => ext.to_str() == Some("md") || ext.to_str() == Some("MD"),
+        Some(ext) => ext.to_string_lossy().to_lowercase() == "md",
     }
 }
