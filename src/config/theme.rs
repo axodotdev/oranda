c871bcf1dcdfbdeb424dd985b77459c786d07e10
Sara Vieira <hey@iamsaravieira.com>
2023-03-10 00:19:04 +0100

    add cupcake theme

--- src/config/theme.rs
+++ src/config/theme.rs
@@ -6,12 +6,14 @@ pub enum Theme {
     Light,
     Dark,
     Hacker,
+    Cupcake,
 }
 
 pub fn css_class(theme: &Theme) -> &str {
     match theme {
         Theme::Dark => "dark",
         Theme::Hacker => "hacker",
+        Theme::Cupcake => "cupcake",
         _ => "light",
     }
 }
