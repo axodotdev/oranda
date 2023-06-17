e60ec103750e4ddae2c5aacb5b67827fd9df8dea
Ashley Williams <ashley666ashley@gmail.com>
2023-06-17 09:09:58 -0500

    fix(funding): remove duplicate logic re funding path

--- src/data/funding.rs
+++ src/data/funding.rs
@@ -1,8 +1,7 @@
-use crate::config::{Config, FundingConfig};
+use crate::config::{FundingConfig, StyleConfig};
 use crate::errors::{OrandaError, Result};
 use crate::site::markdown::to_html;
 use axoasset::LocalAsset;
-use camino::{Utf8Path, Utf8PathBuf};
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
@@ -42,9 +41,9 @@ pub enum FundingContent {
 
 impl Funding {
     /// Creates a new Funding struct by attempting to read from the FUNDING.yml, and the docs file.
-    pub fn new(config: &Config) -> Result<Self> {
-        let path_to_funding = Self::path(&config.funding);
-        let mut funding = match LocalAsset::load_string(&path_to_funding) {
+    pub fn new(funding_cfg: &FundingConfig, style_cfg: &StyleConfig) -> Result<Self> {
+        let path_to_funding = funding_cfg.path.as_ref().expect("if not specified or autodetected, this feature should have been disabled, this is a bug");
+        let mut funding = match LocalAsset::load_string(path_to_funding) {
             Ok(res) => {
                 let parsed_response = parse_response(res)?;
                 Self {
@@ -54,7 +53,7 @@ impl Funding {
             }
             Err(e) => {
                 let warning = OrandaError::FundingLoadFailed {
-                    path: path_to_funding,
+                    path: path_to_funding.into(),
                     details: e,
                 };
                 eprintln!("{:?}", miette::Report::new(warning));
@@ -62,23 +61,11 @@ impl Funding {
             }
         };
         if let Ok(res) = LocalAsset::load_string("funding.md") {
-            let html = to_html(&res, &config.styles.syntax_theme())?;
+            let html = to_html(&res, &style_cfg.syntax_theme())?;
             funding.docs_content = Some(html);
         }
 
         Ok(funding)
-    }
-
-    fn path(config: &Option<FundingConfig>) -> Utf8PathBuf {
-        const FUNDING_FILENAME: &str = "FUNDING.yml";
-        const FUNDING_FILEPATH: &str = "./github";
-
-        if let Some(cfg) = config {
-            if let Some(path) = &cfg.path {
-                return Utf8Path::new(&path).into();
-            }
-        }
-        Utf8Path::new(FUNDING_FILEPATH).join(FUNDING_FILENAME)
     }
 }
 
