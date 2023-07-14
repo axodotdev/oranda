e5c5bf88b82981a1989991d5577085db64aaa7c9
Aria Beingessner <a.beingessner@gmail.com>
2023-07-14 09:05:27 -0400

    feat: document our css class lowering a bit more

--- src/site/oranda_theme.rs
+++ src/site/oranda_theme.rs
@@ -1,6 +1,16 @@
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
+/// The serde definitions here define what CSS class these themes lower to
+///
+/// We're currently transitioning from a system where the light and dark
+/// variants of a theme were basically completely independent, to a system
+/// where we just use an extra "dark" class to modify the light theme
+/// as appropriate.
+///
+/// So in the fullness of time Light and Dark here should probably get
+/// serialized as like "default" and "dark default", just as AxoLight
+/// and AxoDark get serialized as "axo" and "dark axo".
 #[derive(
     Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Hash, Serialize, Deserialize, JsonSchema,
 )]
