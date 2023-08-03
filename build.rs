cdcd78e671ee7c2d4485683dcf148f3d28d83591
liv <shadows_withal@fastmail.com>
2023-08-03 19:21:10 +0200

    fix: run build.rs with a tokio runtime

--- build.rs
+++ build.rs
@@ -3,6 +3,13 @@ use oranda_generate_css::DEFAULT_CSS_OUTPUT_DIR;
 fn main() {
     // Build the CSS on-demand if we're running a release-ish build here (as determined by Cargo)
     if std::env::var("PROFILE").is_ok_and(|v| v == "release") {
+        let runtime = tokio::runtime::Builder::new_multi_thread()
+            .worker_threads(1)
+            .max_blocking_threads(128)
+            .enable_all()
+            .build()
+            .expect("Initializing tokio runtime failed");
+        let _guard = runtime.enter();
         oranda_generate_css::build_css(DEFAULT_CSS_OUTPUT_DIR).unwrap();
     }
 }
