6b54cdfafbc88143fe42d4bd15eeed827b14616b
liv <shadows_withal@fastmail.com>
2023-08-03 11:10:37 +0200

    fix: apply more suggestions

--- build.rs
+++ build.rs
@@ -2,6 +2,5 @@ fn main() {
     // Build the CSS on-demand if we're running a release-ish build here (as determined by Cargo)
     if std::env::var("PROFILE").is_ok_and(|v| v == "release") {
         oranda_generate_css::build_css("oranda-css/dist/").unwrap();
-        println!("cargo:rustc-env=ORANDA_CSS_EXISTS=true");
     }
 }
