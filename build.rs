926b0b2653d9ca7466ea0dd48906e173afb2f65d
liv <shadows_withal@fastmail.com>
2023-08-10 19:56:48 +0200

    fix: apply same condition to build.rs

--- build.rs
+++ build.rs
@@ -2,7 +2,7 @@ use oranda_generate_css::default_css_output_dir;
 
 fn main() {
     // Build the CSS on-demand if we're running a release-ish build here (as determined by Cargo)
-    if std::env::var("PROFILE").is_ok_and(|v| v == "release") {
+    if !cfg!(debug_assertions) {
         let runtime = tokio::runtime::Builder::new_multi_thread()
             .worker_threads(1)
             .max_blocking_threads(128)
