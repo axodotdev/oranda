216509dbcf93b0aa258549cd79bfd542afe71d88
liv <shadows_withal@fastmail.com>
2023-10-10 11:04:28 +0200

    feat: add link to rss feed on changelog page

--- src/site/changelog.rs
+++ src/site/changelog.rs
@@ -10,6 +10,7 @@ use crate::site::{javascript, markdown};
 pub struct ChangelogContext {
     pub releases: Vec<ChangelogRelease>,
     pub has_prereleases: bool,
+    pub has_rss_feed: bool,
     pub os_script: String,
 }
 
@@ -33,6 +34,7 @@ pub fn index_context(
         return Ok(ChangelogContext {
             releases: Vec::new(),
             has_prereleases: false,
+            has_rss_feed: config.components.changelog.clone().is_some_and(|c| c.generate_rss_feed),
             os_script: javascript::build_os_script_path(&config.build.path_prefix),
         });
     }
@@ -44,6 +46,7 @@ pub fn index_context(
     Ok(ChangelogContext {
         releases,
         has_prereleases: context.has_prereleases,
+        has_rss_feed: config.components.changelog.clone().is_some_and(|c| c.generate_rss_feed),
         os_script: javascript::build_os_script_path(&config.build.path_prefix),
     })
 }
