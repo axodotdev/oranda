faa63b2ca9d4bd0e55b7e280c9e6edb35a75354d
Misty De Méo <misty@axo.dev>
2023-11-29 09:45:32 -0800

    feat(cli): config-schema prints to file

--- src/commands/print.rs
+++ src/commands/print.rs
@@ -1,16 +1,27 @@
+use axoasset::LocalAsset;
 use clap::Parser;
 use oranda::errors::*;
 use oranda_generate_css::default_css_output_dir;
 
 #[derive(Debug, Parser)]
-pub struct ConfigSchema {}
+pub struct ConfigSchema {
+    /// Write the config schema to the named file instead of stdout
+    #[clap(long)]
+    pub output: Option<String>,
+}
 
 impl ConfigSchema {
     pub fn run(&self) -> Result<()> {
         let schema = schemars::schema_for!(oranda::config::OrandaLayer);
         let json_schema =
             serde_json::to_string_pretty(&schema).expect("failed to stringify schema!?");
-        println!("{json_schema}");
+
+        if let Some(output) = &self.output {
+            let contents = json_schema + "\n";
+            LocalAsset::write_new(&contents, output)?;
+        } else {
+            println!("{json_schema}");
+        }
         Ok(())
     }
 }
